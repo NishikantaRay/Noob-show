d7af231020b64bab0423b7e1ae4da78c9765b325
Nishikantaray <nishikantaray1@gmail.com>
2022-01-30 17:59:17 +0530

    added

--- js/index.js
+++ js/index.js
@@ -1,48 +1,43 @@
+const hamburgerMenu = document.querySelector("#hamburger-menu");
+const overlay = document.querySelector("#overlay");
+const nav1 = document.querySelector("#nav-1");
+const nav2 = document.querySelector("#nav-2");
+const nav3 = document.querySelector("#nav-3");
+const nav4 = document.querySelector("#nav-4");
+const nav5 = document.querySelector("#nav-5");
+const navItems = [nav1, nav2, nav3, nav4, nav5];
 
-var $window = $(window);
-var $holder = $("#slides-holder");
-var $slides = $holder.find(".slide");
-
-$slides.each(function(index, element) {
-	$(element).css("z-index", 100 + index).after($("<div />", {
-		class: "slide-fake-height"
-	}));
-});
-
-var $fakeHeights = $slides.next(".slide-fake-height");
-var $toReize = $holder.add($slides).add($fakeHeights);
-var scrollFixer = 3;
-
-$window.on("resize", onResize).resize();
-
-$window.on("scroll", function(event) {
-	( !! window.requestAnimationFrame) ? requestAnimationFrame(onScroll) : onScroll();
-});
-
-function onResize(event) {
-	$toReize.css({
-		height: $window.innerHeight() + "px"
-	});
-	onScroll();
+// Control Navigation Animation
+function navAnimation(val1, val2) {
+  navItems.forEach((nav, i) => {
+    nav.classList.replace(`slide-${val1}-${i + 1}`, `slide-${val2}-${i + 1}`);
+  });
 }
 
-function onScroll(event) {
-	
-  var scrollTop = Math.abs($window.scrollTop()) + scrollFixer;
-	
-  $slides.each(function(index, element) {
-		
-		var $slide = $(element);
-		var $fakeHeight = $slide.next(".slide-fake-height");
-		
-		//This can be optimised a lot, but this is just a demo effect
-		var slideTop = $slide.hasClass("fixed") ? $fakeHeight.offset().top : $slide.offset().top;
-		if (slideTop < scrollTop ) {
-			$slide.addClass("fixed");
-			$fakeHeight.addClass("visible");
-		} else {
-			$slide.removeClass("fixed");
-			$fakeHeight.removeClass("visible");
-		}
-	});
+function toggleNav() {
+  // Toggle: Hamburger Open/Close
+  hamburgerMenu.classList.toggle("active");
+
+  //   Toggle: Menu Active
+  overlay.classList.toggle("overlay-active");
+
+  if (overlay.classList.contains("overlay-active")) {
+    // Animate In - Overlay
+    overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
+
+    // Animate In - Nav Items
+    navAnimation("out", "in");
+  } else {
+    // Animate Out - Overlay
+    overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
+
+    // Animate Out - Nav Items
+    navAnimation("in", "out");
+  }
 }
+
+// Events Listeners
+hamburgerMenu.addEventListener("click", toggleNav);
+navItems.forEach((nav) => {
+  nav.addEventListener("click", toggleNav);
+});
