e749b5c0b3fc4d66af571c1a1c590babe8c6d524
Nishikantaray <nishikantaray1@gmail.com>
2022-01-30 16:52:41 +0530

    added modernize

--- js/index.js
+++ js/index.js
@@ -1,35 +1,48 @@
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      &&
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
-  });
+
+var $window = $(window);
+var $holder = $("#slides-holder");
+var $slides = $holder.find(".slide");
+
+$slides.each(function(index, element) {
+	$(element).css("z-index", 100 + index).after($("<div />", {
+		class: "slide-fake-height"
+	}));
+});
+
+var $fakeHeights = $slides.next(".slide-fake-height");
+var $toReize = $holder.add($slides).add($fakeHeights);
+var scrollFixer = 3;
+
+$window.on("resize", onResize).resize();
+
+$window.on("scroll", function(event) {
+	( !! window.requestAnimationFrame) ? requestAnimationFrame(onScroll) : onScroll();
+});
+
+function onResize(event) {
+	$toReize.css({
+		height: $window.innerHeight() + "px"
+	});
+	onScroll();
+}
+
+function onScroll(event) {
+	
+  var scrollTop = Math.abs($window.scrollTop()) + scrollFixer;
+	
+  $slides.each(function(index, element) {
+		
+		var $slide = $(element);
+		var $fakeHeight = $slide.next(".slide-fake-height");
+		
+		//This can be optimised a lot, but this is just a demo effect
+		var slideTop = $slide.hasClass("fixed") ? $fakeHeight.offset().top : $slide.offset().top;
+		if (slideTop < scrollTop ) {
+			$slide.addClass("fixed");
+			$fakeHeight.addClass("visible");
+		} else {
+			$slide.removeClass("fixed");
+			$fakeHeight.removeClass("visible");
+		}
+	});
+}
